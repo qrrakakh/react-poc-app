d4d5ba6c2fc594aaf660535abc55406975402801
Hiroaki KURAMAE <hiroaki.kuramae@gmail.com>
2025-02-23 22:05:48 +0900

    Migrated to React-router v7.

---
+++ src/catchall.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import App from "./App";
+
+export default function Component() {
+  return (
+    <div>
+      <h1>404 Not found.</h1>
+      <p><a href="/">Back to the root.</a></p>
+  </div>
+  );
+}
